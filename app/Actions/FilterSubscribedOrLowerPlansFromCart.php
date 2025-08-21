3908c26f9489ddd79fcecd712960011ef873a04a
Kingdom <obiora@freeki.com>
2025-08-21 05:17:11 +0100

    Filter cart Plan

--- app/Actions/FilterSubscribedOrLowerPlansFromCart.php
+++ app/Actions/FilterSubscribedOrLowerPlansFromCart.php
@@ -17,6 +17,7 @@ class FilterSubscribedOrLowerPlansFromCart
             if ($item->options->type === ProductTypeEnum::VIRTUAL_ADDRESS->value) {
                 $cartPlan = app(CartService::class)->getPlanFromCart();
                 if ($cartPlan) {
+                    dd($cartPlan);
                     $cartPlan = Plan::find($cartPlan->id);
                     if (auth()->user() && auth()->user()->subscribed('default')) // user has a plan 
                     {
