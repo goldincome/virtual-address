e178a950a8c77f9a75a953fa968e7043ac65459e
Kingdom <obiora@freeki.com>
2025-08-21 05:14:59 +0100

    Filter cart Plan

--- app/Actions/FilterSubscribedOrLowerPlansFromCart.php
+++ app/Actions/FilterSubscribedOrLowerPlansFromCart.php
@@ -16,12 +16,14 @@ class FilterSubscribedOrLowerPlansFromCart
         foreach (Cart::content() as $item) {
             if ($item->options->type === ProductTypeEnum::VIRTUAL_ADDRESS->value) {
                 $cartPlan = app(CartService::class)->getPlanFromCart();
-                $cartPlan = Plan::find($cartPlan->id);
-                if (auth()->user() && auth()->user()->subscribed('default')) // user has a plan 
-                {
-                    $subscribedPlan =  auth()->user()->subscription('default')->plan ?? null;
-                    if($cartPlan->id == $subscribedPlan->id || $subscribedPlan->level > $cartPlan->level){
-                        app(CartService::class)->remove($item->rowId);
+                if ($cartPlan) {
+                    $cartPlan = Plan::find($cartPlan->id);
+                    if (auth()->user() && auth()->user()->subscribed('default')) // user has a plan 
+                    {
+                        $subscribedPlan =  auth()->user()->subscription('default')->plan ?? null;
+                        if($cartPlan->id == $subscribedPlan->id || $subscribedPlan->level > $cartPlan->level){
+                            app(CartService::class)->remove($item->rowId);
+                        }
                     }
                 }
             }
